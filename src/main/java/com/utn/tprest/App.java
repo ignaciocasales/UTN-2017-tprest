ecdf98f7019c94834363bc8110bb9a2d3e44d531
ignaciocasales <ignacio.casales@gmail.com>
2017-05-31 16:31:55 -0300

    quickstart

---
+++ src/main/java/com/utn/tprest/App.java
@@ -0,0 +1,13 @@
+package com.utn.tprest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
